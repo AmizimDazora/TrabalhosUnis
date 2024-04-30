8c114dfa56a76a9425a6e5ee610b14ea4989b118
AmizimDazora <enzoreali8@gmail.com>
2024-04-30 20:16:41 -0300

    pipipipopopo

--- Algweb/mysql/servidor.php
+++ Algweb/mysql/servidor.php
@@ -4,4 +4,9 @@ $dbname = "banco_aula";
 $username = "root";
 $password = "";
 
-    $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
+$con = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
+$consulta = "select * from usuarios";
+$resultado = $con->query($consulta);
+while ($linha = $resultado->fetch()){
+    echo $linha["nome"]."-".$linha["email"]."-".$linha["senha"]. PHP_EOL;
+}
