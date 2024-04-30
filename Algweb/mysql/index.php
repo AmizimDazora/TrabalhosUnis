105aadbd4020ae27f64cc36fb365a1c88b05139b
AmizimDazora <enzoreali8@gmail.com>
2024-04-30 20:30:11 -0300

    lalala

--- Algweb/mysql/index.php
+++ Algweb/mysql/index.php
@@ -4,9 +4,12 @@ $dbname = "banco_aula";
 $username = "root";
 $password = "";
 
+echo "<h1>Lista de Usuários</h1>";
+echo "<pre>";
 $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
 $consulta = "select * from usuarios";
 $resultado = $con->query($consulta);
 while ($linha = $resultado->fetch()){
     echo $linha["nome"]." - ".$linha["email"]." - ".$linha["senha"]. PHP_EOL;
 }
+echo "</pre>";
