ff6f1ff1f5f21e00932a088eb5f62b25c81ccfcb
AmizimDazora <enzoreali8@gmail.com>
2024-04-30 21:44:07 -0300

    done

---
+++ Algweb/mysql/cadastro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+<form action="cadastro.php" method="post">
+    <label for="nome">nome</label>
+    <input type="text" name="nome" id="nome">
+    <input type="submit" name="enviar" value="Enviar">
+</form>
+</body>
+</html>
--- Algweb/mysql/index.php
+++ Algweb/mysql/index.php
@@ -1,4 +1,10 @@
 <?php
+
+// php -S localhost:8001
+// para funcionar no localhost
+// localhost:8001
+// como se chama index essa será a pagina padrão.
+
 $dbhost = "localhost";
 $dbname = "banco_aula";
 $username = "root";
@@ -13,3 +19,15 @@ while ($linha = $resultado->fetch()){
     echo $linha["nome"]." - ".$linha["email"]." - ".$linha["senha"]. PHP_EOL;
 }
 echo "</pre>";
+
+$data = [
+    "nome" => "cleito",
+    "email" => "cleitolegal23@yahoo.com",
+    "senha" => "1234"
+];
+$comando = "
+    INSERT INTO usuarios (id, nome, email, senha)
+    VALUES (NULL, :nome, :email, SHA1(:senha))";
+$resultado = $con->prepare($comando);
+$resultado->execute($data);
+echo "<p>Dados Inseridos.</p>";
