453ddb69ae4ad15778c118a64760ebb7d714876b
AmizimDazora <enzoreali8@gmail.com>
2024-05-04 22:11:25 -0300

    wah

--- POO/Tinder/App.java
+++ POO/Tinder/App.java
@@ -14,7 +14,7 @@ public class App {
         System.out.println("eu sou "+gay.getGenero());
         System.out.println("meu cpf é "+gay.getCpf());
         System.out.println("eu amo "+penis.getGenero());
-        System.out.println("");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         System.out.println("eu sou "+lesb.getGenero());
         System.out.println("meu cpf é  "+lesb.getCpf());
         System.out.println("eu amo "+bct.getGenero());
